2fdd9dfc0ed498a9394bf3e4e4c9bac70c56e3b2
Marcos Mendes <77952215+markstali@users.noreply.github.com>
2021-06-02 21:55:57 -0300

    Apaguei o exercício 6

--- code13.05.py
+++ code13.05.py
@@ -95,11 +95,6 @@ print('Casados = ',estado_civil.count('c')) """
    print(i, end = ' ')  """
 
 
-# 6. O Sr. Manoel Joaquim acaba de adquirir uma panificadora e pretende implantar
-# a metodologia da tabelinha, que já é um sucesso na sua loja de 1,99. Você foi
-# contratado para desenvolver o programa que monta a tabela de preços de pães,
-# de 1 até 50 pães, a partir do preço do pão informado pelo usuário, conforme o
-# exemplo abaixo:
 
 
 
